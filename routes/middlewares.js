5faa3090f291595496d344cb4ad4925b62a939f1
RegistryDev <registry.develop@gmail.com>
2021-02-04 17:30:48 +0900

    add jwt(verifyToken)

--- routes/middlewares.js
+++ routes/middlewares.js
@@ -1,3 +1,5 @@
+const jwt = require('jsonwebtoken');
+
 exports.isLoggedIn = (req, res, next) => {
   if (req.isAuthenticated()) {
     next();
@@ -12,5 +14,23 @@ exports.isNotLoggedIn = (req, res, next) => {
   } else {
     const message = encodeURIComponent('Signed in.');
     res.redirect(`/?error=${message}`);
+  }
+};
+
+exports.verifyToken = (req, res, next) => {
+  try {
+    req.decoded = jwt.verify(req.headers.authorization, process.env.JWT_SECRET);
+    return next();
+  } catch (e) {
+    if (e.name === 'TokenExpiredError') {
+      return res.status(419).json({
+        code: 419,
+        message: 'Token has Expired.',
+      });
+    }
+    return res.status(401).json({
+      code: 401,
+      message: 'This token is not valid.'
+    });
   }
 };
